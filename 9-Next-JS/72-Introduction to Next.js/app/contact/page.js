4ee8842d1ab64df7773a7a35d0ba60eb474e8689
Khawaja Zeeshan <khawajazeeshan03488944412@gmail.com>
2025-01-06 16:26:56 +0500

    Web Development Course

---
+++ 9-Next-JS/72-Introduction to Next.js/app/contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contact = () => {
+    return (
+        <div>
+            i am contact pages.....
+        </div>
+    )
+}
+
+export default contact
---
+++ 9-Next-JS/72-Introduction to Next.js/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div>
+            i am a navbar........
+        </div>
+    )
+}
+
+export default navbar
